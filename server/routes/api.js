b9ab4b3a3869ec2d390299276e577db855b35387
Geebergy <obeingilbert3884@gmail.com>
2024-10-29 09:27:18 +0100

    Update mongoose

--- server/routes/api.js
+++ server/routes/api.js
@@ -18,7 +18,10 @@ const uri = process.env.uri;
 
 async function connectToMongoDB() {
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('Connected to MongoDB');
   } catch (error) {
 
